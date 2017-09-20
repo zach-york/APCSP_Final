d727b44fb4e265d7f4b9a8fec39f298e9510d9c2
Zach York <zacharyyork@comcast.net>
2017-09-20 08:00:29 -0600

    Create setup and main command

--- argyle_pattern.py
+++ argyle_pattern.py
@@ -1,5 +1,27 @@
 import turtle
 
 def main():
-	Philip = turtle.Trutle()
+	Philip = turtle.Turtle()
 	Philip.speed(0)
+
+	setup(Philip)
+def setup(Philip):
+	Philip.penup()
+	Philip.left(90)
+	Philip.forward(400)
+	Philip.left(90)
+	Philip.forward(475)
+	Philip.left(180)
+	Philip.pendown()
+
+
+#def top_and_bottom_triangles(Philip):
+
+
+#def side_triangles(Philip):
+
+
+#def inside_diamonds(Philip):
+
+main()
+turtle.done()
