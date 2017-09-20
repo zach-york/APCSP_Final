f7adb7266e46340a58b288963e511356e7a8894b
Zach York <zacharyyork@comcast.net>
2017-09-20 08:33:36 -0600

    Add triangle function to shapes and make 5 triangles

--- argyle_pattern.py
+++ argyle_pattern.py
@@ -1,21 +1,29 @@
 import turtle
+from shapes import triangle
 
 def main():
 	Philip = turtle.Turtle()
 	Philip.speed(0)
 
 	setup(Philip)
+	top_and_bottom_triangles(Philip)
+
 def setup(Philip):
 	Philip.penup()
 	Philip.left(90)
 	Philip.forward(400)
 	Philip.left(90)
-	Philip.forward(475)
+	Philip.forward(530)
 	Philip.left(180)
 	Philip.pendown()
 
 
-#def top_and_bottom_triangles(Philip):
+
+def top_and_bottom_triangles(Philip):
+	for count in range(5):
+		Philip.color("red")
+		Philip.triangle(Philip, "red")
+
 
 
 #def side_triangles(Philip):
